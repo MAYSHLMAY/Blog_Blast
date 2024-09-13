b3cfa9c5aff8d684363d93b5cff2700d40fc3333
Mike <educationforlife424@gmail.com>
2024-09-13 18:46:12 +0300

    Done

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,7 +7,9 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEYY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
 };
 
 
